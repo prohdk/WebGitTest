c9094be254d750586aa1d27d2bfee192a3412614
h.dham@kalmate.net <h.dham@kalmate.net>
2022-05-24 11:18:48 +0900

    4444

--- WebGitTest/Startup.cs
+++ WebGitTest/Startup.cs
@@ -32,7 +32,7 @@ namespace WebGitTest
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World2222332!");
+                    await context.Response.WriteAsync("Hello World4444!");
                 });
             });
         }
